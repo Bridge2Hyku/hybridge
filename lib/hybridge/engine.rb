6bd534281c9ad1f306f8a51c03986f4b14d14921
Sean Watkins <seanlw@gmail.com>
2018-08-15 02:51:12 -0500

    Add HyBridge option to Hyku sidebar in Dashboard

--- lib/hybridge/engine.rb
+++ lib/hybridge/engine.rb
@@ -1,5 +1,13 @@
 module Hybridge
   class Engine < ::Rails::Engine
     isolate_namespace Hybridge
+    
+    config.after_initialize do
+      hybridge_root = root.to_s
+      paths = ActionController::Base.view_paths.collect{|p| p.to_s}
+      paths = paths.unshift(hybridge_root + '/app/views')
+      ActionController::Base.view_paths = paths
+      Rails.logger.info "paths: #{paths}"
+    end
   end
 end
