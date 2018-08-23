db819fcc8cfd13df5f30f0d26e39e8b59ab01f1f
Sean Watkins <slwatkins@uh.edu>
2018-08-23 13:35:08 -0500

    Update default job queue

--- app/jobs/hybridge/ingest_package_job.rb
+++ app/jobs/hybridge/ingest_package_job.rb
@@ -1,7 +1,7 @@
 require 'hybridge/batch'
 module Hybridge
   class IngestPackageJob < ApplicationJob
-    queue_as :hybridge_ingest_package
+    queue_as Hyrax.config.ingest_queue_name
 
     def perform(package_location, current_user)
       Hybridge::Batch::Ingest.new(package_location, current_user)
