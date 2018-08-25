ba85a04e0050c3f8f4d960d799e211635fe06228
Sean Watkins <seanlw@gmail.com>
2018-08-25 13:38:38 -0500

    Inject the HyBridge Ingest link into the Hyku sidebar view
    Inject the HyBridge Ingest menu option into the sidebar view instead of overwriting the view completely. This allows for additional gems within the dashboard to be used.

--- lib/generators/hybridge/install/install_generator.rb
+++ lib/generators/hybridge/install/install_generator.rb
@@ -4,4 +4,17 @@ class Hybridge::InstallGenerator < Rails::Generators::Base
       %(\n  mount Hybridge::Engine => '/hybridge'\n)
     end
   end
+
+  def inject_dashboard_link
+    file_path = "app/views/hyrax/dashboard/_sidebar.html.erb"
+    if File.file?(file_path)
+      insert_into_file file_path, after: /menu\.nav_link\(hyrax\.my_works_path.*?<% end %>/m do
+        "\n\n  <%= menu.nav_link(hybridge.root_path,\n" \
+        "                        also_active_for: hyrax.dashboard_works_path) do %>\n" \
+        "    <span class=\"fa fa-magic\"></span> <span class=\"sidebar-action-text\"><%= t('hybridge.admin.sidebar.ingest') %></span>\n" \
+        "  <% end %>\n"
+      end
+    end
+  end
+
 end
--- lib/hybridge/engine.rb
+++ lib/hybridge/engine.rb
@@ -1,12 +1,5 @@
 module Hybridge
   class Engine < ::Rails::Engine
     isolate_namespace Hybridge
-    
-    config.after_initialize do
-      hybridge_root = root.to_s
-      paths = ActionController::Base.view_paths.collect{|p| p.to_s}
-      paths = paths.unshift(hybridge_root + '/app/views')
-      ActionController::Base.view_paths = paths
-    end
   end
 end
